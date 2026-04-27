f5e5ec2d6616660f67f66fbeee77be6bd07dd362
ddjj8472 <ddjj0911@gmail.com>
2026-04-27 15:10:22 +0900

    Update components.py

--- components.py
+++ components.py
@@ -4,6 +4,5 @@ def render_user_message(query):
     st.markdown(f'<div class="user-msg">질문: {query}</div>', unsafe_allow_html=True)
 
 def render_ai_report(response_text):
-    st.markdown('<div class="report-card">', unsafe_allow_html=True)
-    st.markdown(response_text)
-    st.markdown('</div>', unsafe_allow_html=True)
+    formatted_text = response_text.replace("\n", "<br>")
+    st.markdown(f'<div class="report-card">{formatted_text}</div>', unsafe_allow_html=True)
